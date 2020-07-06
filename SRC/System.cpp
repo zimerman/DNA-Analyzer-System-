afdfa7a24714a103f0a9e60e7718ba2c02a25066
zimerman <zr@sem-wolf.org>
2020-07-06 02:52:45 -0700

    support a new command

--- SRC/System.cpp
+++ SRC/System.cpp
@@ -5,6 +5,7 @@
 #include "System.h"
 #include "Icommand.h"
 #include "ManageCommand.h"
+
 void System::startApp()
 {
     while(1)
@@ -13,6 +14,7 @@ void System::startApp()
         ParamCommand paramCommand(k.getData());
         Icommand* icommand = ManageCommand::createCtorCommand(paramCommand);
         KeyBoardWriter key;
+        //icommand->run(paramCommand);
         key.write(icommand->run(paramCommand));
     }
 }
