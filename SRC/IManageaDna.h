3ad9349bb2e1688187545cff3f58abd97cb36b6e
zimerman <zr@sem-wolf.org>
2020-07-08 00:48:29 -0700

    add Interface class-IManageDna that inheritance from ICommand

---
+++ SRC/IManageaDna.h
@@ -0,0 +1,18 @@
+//
+// Created by a on 7/7/20.
+//
+
+#ifndef SRC_IMANAGEADNA_H
+#define SRC_IMANAGEADNA_H
+
+#include "ParamCommand.h"
+#include "DataDNA.h"
+#include "DNA.h"
+#include "Icommand.h"
+
+class IManageaDna: public Icommand
+{
+
+};
+
+#endif //SRC_IMANAGEADNA_H
