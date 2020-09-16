c203d950f37f8f503ae2db81df8590b16a758f3b
zimerman <zr@sem-wolf.org>
2020-09-16 01:10:58 -0700

    add len command

---
+++ SRC/Controller/Commands/Len.cpp
@@ -0,0 +1,24 @@
+//
+// Created by a on 9/16/20.
+//
+
+#include "Len.h"
+#include "../Auxiliaryfunctions.h"
+
+bool Len::isValid(const Paramcommand& param)
+{
+    return param.getParam().size() == 2 &&  param.getParam()[1][0] == '#';
+}
+
+void Len::run(const Iwriter& writer, dataDNA& containerDna,const Paramcommand& param)
+{
+    if(!isValid(param))
+        throw std::invalid_argument("command not found");
+    size_t len = containerDna.findInIdMap(castToSize(param.getParam()[1].substr(1)))->getDna().length();
+    print(writer, len);
+}
+
+void Len::print(const Iwriter& writer, size_t len)
+{
+    writer.write(castToString(len));
+}
---
+++ SRC/Controller/Commands/Len.h
@@ -0,0 +1,23 @@
+//
+// Created by a on 9/16/20.
+//
+
+#ifndef DNA_ANALYZER_PROJECT_LEN_H
+#define DNA_ANALYZER_PROJECT_LEN_H
+
+#include "../ImanageDna.h"
+#include "../../View/rawDnaWriter.h"
+
+class Len: public ImanageDna{
+
+public:
+    /*virtual*/~Len(){}
+    /*virtual*/void run(const Iwriter& writer, dataDNA& containerDna,const Paramcommand& param);
+
+private:
+    bool isValid(const Paramcommand& param);
+    void print(const Iwriter& writer, size_t len);
+};
+
+
+#endif //DNA_ANALYZER_PROJECT_LEN_H
