914345e4b2435bfde948d5c21ec83378a2c06dcb
zimerman <zr@sem-wolf.org>
2020-09-17 00:35:42 -0700

    add count command

---
+++ SRC/Controller/Commands/Count.cpp
@@ -0,0 +1,72 @@
+//
+// Created by a on 9/16/20.
+//
+
+#include "Count.h"
+#include "../Auxiliaryfunctions.h"
+
+bool Count::isValid(const Paramcommand& param)
+{
+    return param.getParam().size() == 3 && (param.getParam()[1][0] == '@' || param.getParam()[1][0] == '#');
+}
+
+void Count::run(const Iwriter& writer, Ireader& reader,dataDNA& containerDna,const Paramcommand& param)
+{
+    if(!isValid(param))
+        throw std::invalid_argument("command not found");
+    size_t idDnaOrginal;
+    size_t idDnaSub = 0;
+    size_t NumIndexFind;
+    if(param.getParam()[1][0]=='@')
+    {
+        if(!containerDna.isexistName(param.getParam()[1].substr(1)))
+        {
+            writer.write("name of DNA not found");
+            return;
+        }
+        idDnaOrginal = containerDna.findIdByName(param.getParam()[1].substr(1));
+    }
+
+    else
+    {
+        idDnaOrginal = castToSize(param.getParam()[1].substr(1));
+        if(!containerDna.isexistId(idDnaOrginal))
+        {
+            writer.write("id of DNA not found");
+            return;
+        }
+    }
+    if(param.getParam()[2][0]=='@')
+    {
+        if(!containerDna.isexistName(param.getParam()[2].substr(1)))
+        {
+            writer.write("name of DNA not found");
+            return;
+        }
+        idDnaSub = containerDna.findIdByName(param.getParam()[2].substr(1));
+    }
+
+    else if(param.getParam()[2][0]=='#')
+    {
+        idDnaSub = castToSize(param.getParam()[2].substr(1));
+        if(!containerDna.isexistId(idDnaSub))
+        {
+            writer.write("id of DNA not found");
+            return;
+        }
+    }
+    if(idDnaSub)
+    {
+        NumIndexFind = containerDna.findInIdMap(idDnaOrginal)->getDna().count(containerDna.findInIdMap(idDnaSub)->getDna());
+    }
+    else
+    {
+        NumIndexFind = containerDna.findInIdMap(idDnaOrginal)->getDna().count(param.getParam()[2]);
+    }
+    print(writer, castToString(NumIndexFind));
+}
+
+void Count::print(const Iwriter& writer, const std::string& string)
+{
+    writer.write(string);
+}
---
+++ SRC/Controller/Commands/Count.h
@@ -0,0 +1,20 @@
+//
+// Created by a on 9/16/20.
+//
+
+#ifndef DNA_ANALYZER_PROJECT_COUNT_H
+#define DNA_ANALYZER_PROJECT_COUNT_H
+
+#include "Icommand.h"
+
+class Count:public SequenceAnalysis {
+public:
+    /*virtual*/~Count(){};
+    /*virtual*/void run(const Iwriter& writer, Ireader& reader,dataDNA& containerDna,const Paramcommand& param);
+private:
+    bool isValid(const Paramcommand& param);
+    void print(const Iwriter& writer, const std::string& string);
+};
+
+
+#endif //DNA_ANALYZER_PROJECT_COUNT_H
