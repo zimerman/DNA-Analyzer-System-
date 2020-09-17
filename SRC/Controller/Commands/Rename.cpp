da4e16a1c9f4c3b93aeb8386079f6b912c264807
zimerman <zr@sem-wolf.org>
2020-09-17 01:06:03 -0700

    add rename command

---
+++ SRC/Controller/Commands/Rename.cpp
@@ -0,0 +1,41 @@
+//
+// Created by a on 9/16/20.
+//
+
+#include "Rename.h"
+#include "../Auxiliaryfunctions.h"
+
+bool Rename::isValid(const Paramcommand& param)
+{
+    return param.getParam().size() == 3 && (param.getParam()[1][0] == '@' || param.getParam()[1][0] == '#') && param.getParam()[2][0] == '@';
+}
+
+void Rename::run(const Iwriter& writer, Ireader& reader,dataDNA& containerDna,const Paramcommand& param)
+{
+    if(!isValid(param))
+        throw std::invalid_argument("command not found");
+    size_t idDna;
+    if(param.getParam()[1][0]=='@')
+    {
+        if(!containerDna.isexistName(param.getParam()[1].substr(1)))
+        {
+            writer.write("name of DNA not found");
+            return;
+        }
+        idDna = containerDna.findIdByName(param.getParam()[1].substr(1));
+    }
+
+    else
+    {
+        idDna = castToSize(param.getParam()[1].substr(1));
+        if(!containerDna.isexistId(idDna))
+        {
+            writer.write("id of DNA not found");
+            return;
+        }
+    }
+    std::cout<<containerDna.findInIdMap(idDna)->getName();
+    std::cout<<param.getParam()[2].substr(1)<<"\n";
+    containerDna.setName(idDna, param.getParam()[2].substr(1));
+}
+
---
+++ SRC/Controller/Commands/Rename.h
@@ -0,0 +1,21 @@
+//
+// Created by a on 9/16/20.
+//
+
+#ifndef DNA_ANALYZER_PROJECT_RENAME_H
+#define DNA_ANALYZER_PROJECT_RENAME_H
+
+#include "Icommand.h"
+#include "../ImanageDna.h"
+
+class Rename:public ImanageDna{
+
+public:
+    /*virtual*/~Rename(){}
+    /*virtual*/void run(const Iwriter& writer, Ireader& reader,dataDNA& containerDna,const Paramcommand& param);
+private:
+    bool isValid(const Paramcommand& param);
+};
+
+
+#endif //DNA_ANALYZER_PROJECT_RENAME_H
